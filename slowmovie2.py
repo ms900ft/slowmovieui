c91fbee3257ea952eeb5c94007efac5eb31744b5
ms <ms@alter-fisch.de>
2021-04-16 08:51:37 +0200

    add file after upload

--- slowmovie2.py
+++ slowmovie2.py
@@ -105,20 +105,24 @@ def update_list():
 
 
 @app.route('/files', methods=['POST'])
+@cross_origin()
 def add_file():
     # check if the post request has the file part
+    print( request.data)
     if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
+        print('No file part')
+        print(request)
+        return "bad request", 400
     file = request.files['file']
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
+        print ('No selected file')
+        return "bad request", 400
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        player.AddFile(filename)
         return "1", 201
 
     return "bad request", 400
